0ef9c078a804d94631b6e38241625577572bbf1c
cars2614 <cars2614@gmail.com>
2024-04-02 09:08:02 -0500

    card pagina inicial Factura, Clientes y Empleados

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\DetallesEstado;
+
+use App\Models\Facturas;
+use App\Models\Clientes;
+use App\Models\Empleado;
+
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +30,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $numeroFactura = DB::select('SELECT COUNT(*)AS cantidad FROM facturas');
+        $numeroClientes = DB::select('SELECT COUNT(*)AS cantidad FROM clientes');
+        $numeroEmpleados = DB::select('SELECT COUNT(*)AS cantidad FROM empleados');
+        
+
+        //dd($numeroFactura);
+        return view('home', compact('numeroFactura', 'numeroClientes', 'numeroEmpleados'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,44 +14,132 @@
 
 
     <div class="row">
+        {{-- Tarjeta Registros --}}
         <div class="col-lg-3 col-md-6 col-sm-6">
             <div class="card card-stats">
-                <div class="card-header card-header-warning card-header-icon">
+                <div class="card-header card-header-econdary card-header-icon">
                     <div class="card-icon">
-                        <i class="fas fa-user fa-2x"></i>
+
+
+                        <i class="fas fa-users"></i>
                     </div>
-                    <a href="#" class="card-category text-warning font-weight-bold">
-                        Usuarios
+                    <a href="{{ url('/facturas/create') }}" class="card-category text-dark font-weight-bold">
+                        Totos Nuestros Registros
                     </a>
-                    <h3 class="card-title">okokokok</h3>
+
+                    <h2>
+
+                        @foreach ($numeroFactura as $num)
+                            <b>
+                                {{ $num->cantidad }}
+                            </b>
+                        @endforeach
+
+                    </h2>
+
                 </div>
 
-                <div 
-                class="card-footer bg-warning text-white">
+                <div class="card-footer bg-dark text-dark">
                 </div>
-                
+
             </div>
         </div>
+        {{-- Fin Tarjeta --}}
+
+
+
+        {{-- Tarjeta Clientes --}}
+        <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+                <div class="card-header card-header-econdary card-header-icon">
+                    <div class="card-icon">
+
+
+                        <i class="fas fa-user-check"></i>
+                    </div>
+                    <a href="{{ url('/facturas/create') }}" class="card-category text-dark font-weight-bold">
+                        Totos Nuestros Clientes
+                    </a>
+
+                    <h2>
+
+                        @foreach ($numeroClientes as $num)
+                            <b>
+                                {{ $num->cantidad }}
+                            </b>
+                        @endforeach
+
+                    </h2>
+
+                </div>
+
+                <div class="card-footer bg-dark text-dark">
+                </div>
+
+            </div>
+        </div>
+        {{-- Fin Tarjeta --}}
+
+
+
+        {{-- Tarjeta Empleados --}}
+        <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+                <div class="card-header card-header-econdary card-header-icon">
+                    <div class="card-icon">
+
+
+                        <i class="fas fa-id-card-alt"></i>
+                    </div>
+                    <a href="{{ url('/facturas/create') }}" class="card-category text-dark font-weight-bold">
+                        Totos Nuestros Empleados
+                    </a>
+
+                    <h2>
+
+                        @foreach ($numeroEmpleados as $num)
+                            <b>
+                                {{ $num->cantidad }}
+                            </b>
+                        @endforeach
+
+                    </h2>
+
+                </div>
+
+                <div class="card-footer bg-dark text-dark">
+                </div>
+
+            </div>
+        </div>
+        {{-- Fin Tarjeta --}}
+
+
+
+
     </div>
+
+
+
 
 
 @stop
 
 @section('css')
-   <link rel="stylesheet" href="{{ asset('/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
 @stop
 
 @section('js')
     <script>
         /* 
-          se comenta para que no salga la alerta al inicio
-          solo es una prueba de que funciona bien       
-            Swal.fire(
-                'Bienvenid@!',
-                'Ejemplo alert2!',
-                'success'
-            ) */
+                                                          se comenta para que no salga la alerta al inicio
+                                                          solo es una prueba de que funciona bien       
+                                                            Swal.fire(
+                                                                'Bienvenid@!',
+                                                                'Ejemplo alert2!',
+                                                                'success'
+                                                            ) */
     </script>
 @stop
 
